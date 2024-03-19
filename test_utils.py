0903f6b240352f4209221de3d2bc4be047b3dd75
Reda <r.elmakroum@aui.ma>
2024-03-19 16:25:49 +0100

    Code Update

--- test_utils.py
+++ test_utils.py
@@ -2,3 +2,4 @@ from utils import subtract
 
 def test_subtract():
     assert subtract(6,3) == 3
+    assert subtract(9,2) == 7
--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 def subtract(a, b):
-    print("&")
     return a - b
 
 print(subtract(6,3))

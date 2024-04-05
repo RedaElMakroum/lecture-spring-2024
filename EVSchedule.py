c6ef465a5bbaa11d1b6cea45292a9b9ecc167ace
Reda <r.elmakroum@aui.ma>
2024-04-05 14:12:53 +0200

    Run Ruff

--- EVSchedule.py
+++ EVSchedule.py
@@ -3,27 +3,26 @@ import pulp
 import copy
 
 
-def minimize_cost(prices,arrival_time,SOC, H):
-
-    OptimalBatteryCapacity= 60
+def minimize_cost(prices, arrival_time, SOC, H):
+    OptimalBatteryCapacity = 60
     electricity_cost = prices
 
-# Make a deep copy
+    # Make a deep copy
     electricity_cost_clean = copy.deepcopy(prices)
 
-# Fetch what's the usual date on which the car starts charging and what's the SOC when it starts
-# This is to generate an estimation of the cycle time for that day
+    # Fetch what's the usual date on which the car starts charging and what's the SOC when it starts
+    # This is to generate an estimation of the cycle time for that day
 
     # Define the problem
     problem = pulp.LpProblem("Minimize_Electricity_Cost", pulp.LpMinimize)
     # Create a list of time slots
     time_slots = list(range(24))
     # Define the decision variables
-    x = pulp.LpVariable.dicts("operation", time_slots, cat='Binary')
-    for i in range(0,arrival_time):
+    x = pulp.LpVariable.dicts("operation", time_slots, cat="Binary")
+    for i in range(0, arrival_time):
         electricity_cost[i] = 120
 
-    for i in range(H,24):
+    for i in range(H, 24):
         electricity_cost[i] = 120
 
     # Objective function
@@ -31,16 +30,16 @@ def minimize_cost(prices,arrival_time,SOC, H):
 
     # Constraint: Operation Cycle
     # problem += pulp.lpSum([x[i] for i in time_slots]) == 1.46
-    
-    
-    
+
     if SOC < OptimalBatteryCapacity:
-        problem += pulp.lpSum([x[i] for i in time_slots]) == (OptimalBatteryCapacity-SOC)/22
+        problem += (
+            pulp.lpSum([x[i] for i in time_slots])
+            == (OptimalBatteryCapacity - SOC) / 22
+        )
     else:
         problem += pulp.lpSum([x[i] for i in time_slots]) == 0
-        
+
     # Constraint: After Arrival and Before 5 AM
-    
 
     # for i in range(21):
     #     problem += x[i+3] <= x[i]  # If operation starts at hour i, it must continue for the next 3 hours
@@ -58,31 +57,30 @@ def minimize_cost(prices,arrival_time,SOC, H):
 
     fig, ax1 = plt.subplots()
 
-    color = 'tab:red'
-    ax1.set_xlabel('Time Slot')
-    ax1.set_ylabel('Prices', color=color)
+    color = "tab:red"
+    ax1.set_xlabel("Time Slot")
+    ax1.set_ylabel("Prices", color=color)
     ax1.plot(electricity_cost_clean, color=color)
-    ax1.tick_params(axis='y', labelcolor=color)
-    ax1.plot(electricity_cost_clean, color=color, label='Prices')
+    ax1.tick_params(axis="y", labelcolor=color)
+    ax1.plot(electricity_cost_clean, color=color, label="Prices")
 
-
-    ax2 = ax1.twinx()  
-    color = 'tab:blue'
-    ax2.set_ylabel('Operation Schedule', color=color)  
+    ax2 = ax1.twinx()
+    color = "tab:blue"
+    ax2.set_ylabel("Operation Schedule", color=color)
     ax2.plot(operation_schedule, color=color)
-    ax2.tick_params(axis='y', labelcolor=color)
-    ax2.plot(operation_schedule, color=color, label='Optimal Charging Schedule')
-    ax2.fill_between(range(len(operation_schedule)), operation_schedule, color='blue', alpha=0.25)
+    ax2.tick_params(axis="y", labelcolor=color)
+    ax2.plot(operation_schedule, color=color, label="Optimal Charging Schedule")
+    ax2.fill_between(
+        range(len(operation_schedule)), operation_schedule, color="blue", alpha=0.25
+    )
 
     fig.legend(loc="upper right")
 
-    fig.tight_layout()  
+    fig.tight_layout()
     plt.show()
-
 
     return total_cost
 
+
 # Call the function with the prices
 # minimize_cost(helper.prices)
-
-
